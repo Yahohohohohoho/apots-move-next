6b7cc627344c279f7cd9c7c247a9a82009e7a0e1
han <hanbu97@proton.me>
2024-03-06 20:42:23 +0800

    feat: connect wallet style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { AppContext } from "../components/wallet/app_context";
 import "./globals.css";
+import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
