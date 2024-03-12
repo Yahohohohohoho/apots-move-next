72300745460e38ff347f98c1f452332c33277433
han <hanbu97@proton.me>
2024-03-12 12:21:19 +0800

    feat: add shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        {/* {children} */}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
